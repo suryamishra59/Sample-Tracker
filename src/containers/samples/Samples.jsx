6c2ce69b7549e7c1a104754a4ea6bbe6090952f9
Surya Deep Mishra <surya.mishra@xoriant.com>
2021-04-18 21:40:57 +0530

    Added QR and Sample seperate page

---
+++ src/containers/samples/Samples.jsx
@@ -0,0 +1,60 @@
+import { Button, Divider } from '@material-ui/core';
+import React, { useState, useEffect, useContext } from 'react';
+import { Header, Loader, SampleTable } from '../../components';
+import { downloadReport, getAllSamples } from '../../server';
+import UserContext from '../../UserContext';
+import './Samples.scss';
+
+function Samples(props) {
+    const [state, setstate] = useState({
+        samples: []
+    });
+    const [isLoading, setisLoading] = useState(false);
+    const { enqueueSnackbar, isMobile, first_name, role } = useContext(UserContext);
+
+    useEffect(() => {
+        getSamples().then();
+    }, []);
+
+    const getSamples = async _ => {
+        setisLoading(true);
+        try {
+            const resp = await getAllSamples();
+            setstate({ ...state, samples: resp.data });
+        } catch (error) {
+            enqueueSnackbar && enqueueSnackbar(error, {
+                variant: "error"
+            });
+        }
+        setisLoading(false);
+    };
+
+    const downloadSampleReport = async downloadable_file_id => {
+        setisLoading(true);
+        try {
+            const resp = await downloadReport(downloadable_file_id);
+            setisLoading(false);
+            const docWin = window.open();
+            docWin.location.href = resp.data.download_link.url;
+        } catch (error) {
+            enqueueSnackbar && enqueueSnackbar(error, {
+                variant: "error"
+            });
+        }
+        setisLoading(false);
+    };
+
+    return (
+        <>
+            <Loader isLoading={isLoading} />
+            <Header heading="My Samples" {...props} />
+            <div className="samples-wrapper flex flex-c-flow flex-v-centered">
+                <div className="flex" style={{ marginTop: '5em', width: isMobile ? '95%' : '70%' }} id="#my_samples">
+                    <SampleTable samples={state.samples} history={props.history} />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Samples;
